45555551dc219e096457d62f347e60647d013142
kamalyes <501893067@qq.com>
2024-11-07 18:27:02 +0800

    :fire: feat: 修复signature

--- pkg/zero/signature.go
+++ pkg/zero/signature.go
@@ -2,7 +2,7 @@
  * @Author: kamalyes 501893067@qq.com
  * @Date: 2024-11-07 15:50:58
  * @LastEditors: kamalyes 501893067@qq.com
- * @LastEditTime: 2024-11-07 18:07:55
+ * @LastEditTime: 2024-11-07 18:25:35
  * @FilePath: \go-config\pkg\zero\signature.go
  * @Description:
  *
@@ -23,16 +23,11 @@ type PrivateKeyConf struct {
 	KeyFile     string `json:"key-file" mapstructure:"key-file" yaml:"key_file"`          // 私钥文件路径
 }
 
-// SignatureConf 是一个签名配置
-type SignatureConf struct {
+// Signature 是一个签名配置
+type Signature struct {
 	Strict      bool             `json:"strict" mapstructure:"strict" yaml:"strict" default:"false"`   // 是否严格验证
 	Expiry      time.Duration    `json:"expiry" mapstructure:"expiry" yaml:"expiry" default:"1h"`      // 签名过期时间
 	PrivateKeys []PrivateKeyConf `json:"private-keys" mapstructure:"private-keys" yaml:"private_keys"` // 私钥列表
-}
-
-// Signature 表示签名配置
-type Signature struct {
-	Algorithm SignatureConf `mapstructure:"algorithm" yaml:"algorithm" json:"algorithm"` // 签名算法
 }
 
 // NewSignature 创建一个新的 Signature 实例
@@ -46,25 +41,29 @@ func NewSignature(opt *Signature) *Signature {
 }
 
 // Clone 返回 Signature 配置的副本
-func (e *Signature) Clone() internal.Configurable {
+func (s *Signature) Clone() internal.Configurable {
 	return &Signature{
-		Algorithm: e.Algorithm,
+		Strict:      s.Strict,
+		Expiry:      s.Expiry,
+		PrivateKeys: s.PrivateKeys,
 	}
 }
 
 // Get 返回 Signature 配置的所有字段
-func (e *Signature) Get() interface{} {
-	return e
+func (s *Signature) Get() interface{} {
+	return s
 }
 
 // Set 更新 Signature 配置的字段
-func (e *Signature) Set(data interface{}) {
+func (s *Signature) Set(data interface{}) {
 	if configData, ok := data.(*Signature); ok {
-		e.Algorithm = configData.Algorithm
+		s.Strict = configData.Strict
+		s.Expiry = configData.Expiry
+		s.PrivateKeys = configData.PrivateKeys
 	}
 }
 
 // Validate 验证 Signature 配置的有效性
-func (e *Signature) Validate() error {
-	return internal.ValidateStruct(e)
+func (s *Signature) Validate() error {
+	return internal.ValidateStruct(s)
 }
